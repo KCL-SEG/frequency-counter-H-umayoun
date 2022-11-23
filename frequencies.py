2d7f25a88c3a34c9ee073e45b468ecddbbfc7fef
Humayoun <humayoun.mahmood@kcl.ac.uk>
2022-11-23 10:57:22 +0000

    first attempt

--- frequencies.py
+++ frequencies.py
@@ -3,5 +3,14 @@
 
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+    for i in range(0, len(items) - 1):
+        if frequencies.get(items(i)) is None:
+            frequencies[items(i)] = 1
+            i = i+1
+        else:
+            frequencies[items(i)] = frequencies.get(items(i)) + 1
+            i = i+1
+        
+
+
     return frequencies
