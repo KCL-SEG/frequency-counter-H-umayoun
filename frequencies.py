0643587ac9042ae0f7d80b18b85c65394f1de973
Humayoun <humayoun.mahmood@kcl.ac.uk>
2022-11-23 13:32:56 +0000

    final

--- frequencies.py
+++ frequencies.py
@@ -3,15 +3,20 @@
 
 def frequencies(items):
     frequencies = {}
-    for i in range(0, len(items) - 1):
-        str = items(i).toString()
-        if frequencies.get(str) is None:
-            frequencies[str] = 1
+    for i in range(0, len(items)):
+        item = str(items[i])
+        if frequencies.get(item) is None:
+            frequencies[item] = 1
             i = i+1
         else:
-            frequencies[str] = frequencies.get(str) + 1
+            frequencies[item] = frequencies.get(item) + 1
             i = i+1
-        
+    
+    print(frequencies)
 
 
     return frequencies
+
+frequencies([1,2,3,4,4,4,4, 'Hello', 'Hello'])
+
+
